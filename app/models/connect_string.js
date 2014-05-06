82c64882f20a2a287f3db89f4ddfcc3187f56186
druyle <dave.ruyle@gmail.com>
2014-05-06 11:49:58 -0700

    added new model for Users and db connection string

---
+++ app/models/connect_string.js
@@ -0,0 +1,21 @@
+var mongodb = require('mongodb')
+  , MongoClient = mongodb.MongoClient
+MongoClient.connect("mongodb://fifa-admin:javacrash1@oceanic.mongohq.com:10068/fifa_madness/", function(err, db) {
+  //var collection = db.collection('fifa_users');
+  if(err){
+    console.log(err);
+    //console.log(collection);
+    console.log("Connection Failed Via Connection String.");
+  } else {
+    console.log("Connected Via Connection String . . .");
+    //db.logout(function(err, result) {
+      //if(!err){
+        //console.log("Logged out Via Connection String . . .");
+      }
+      //db.close();
+      //console.log("Connection closed . . .");
+    //});
+  }
+});
+
+//mongodb://fifa-admin:javacrash1@oceanic.mongohq.com:10068/fifa-madness
---
+++ app/models/doc_find.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect("mongodb://fifa-admin:javacrash1@oceanic.mongohq.com:10068/fifa_madness/", function(err, db) {
+  var myDB = db.db("fifa");
+  myDB.collection("fifa_users", function(err, fifa_users){
+    fifa_users.find(function(err, items){
+      items.toArray(function(err, itemArr){
+        console.log("Document Array: ");
+        console.log(itemArr);
+      });
+    });
+    fifa_users.find(function(err, items){
+      items.each(function(err, item){
+        if(item){
+          console.log("Singular Document: ");
+          console.log(item);
+        }
+      });
+    });
+    fifa_users.findOne({user_id:12}, function(err, item){
+      console.log("Found One: ");
+      console.log(item);
+    });
+  });
+  setTimeout(function(){ db.close(); }, 3000);
+});
