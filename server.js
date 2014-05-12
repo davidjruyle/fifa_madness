3a3ea36192a3857f1adaff6a3e09316bab1b42e1
druyle <dave.ruyle@gmail.com>
2014-05-12 16:41:50 -0700

    added new modules and beginning of authentication

--- server.js
+++ server.js
@@ -2,24 +2,37 @@
 var express = require('express');
 var app     = express();
 var mongoose= require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var port = process.env.PORT || 8080;
+var configDB = require('./config/database.js');
+
 
 // configuration ===========================================
-	
+mongoose.connect(configDB.url);
+
 // config files
 var db = require('./config/db');
+
+require('./config/passport')(passport);
 
 var port = process.env.PORT || 4000; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 app.configure(function() {
+	app.use(express.logger('dev'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser()); 	
 	app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-	app.use(express.logger('dev')); 					// log every request to the console
-	app.use(express.bodyParser()); 						// pull information from html in POST
+	app.use(express.session({secret:'ilovescotchscotchyscotchscotch'}));					
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(flash());					// pull information from html in POST
 	app.use(express.methodOverride()); 					// simulate DELETE and PUT
 });
 
 // routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+require('./app/routes')(app,passport); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
